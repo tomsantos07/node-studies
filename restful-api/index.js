fbd92316988d81c0038a57d301a1bc71216cc0bf
tomsantos07 <tomsantos07>
2022-04-02 17:20:02 -0300

    get the http method

--- restful-api/index.js
+++ restful-api/index.js
@@ -16,11 +16,14 @@ const server = http.createServer(function(req, res) {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\+|\/+$/g,'');
 
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
   // Send the response
   res.end('Hello World!\n');
 
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath);
+  console.log('Request received on path: ' + trimmedPath + ' with method: ' + method);
   
 });
 
