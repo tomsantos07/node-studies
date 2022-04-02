5649cd20a627c01a1e49423361f7d449b20d70c7
tomsantos07 <tomsantos07>
2022-04-02 17:39:08 -0300

    getting the query string as an object

--- restful-api/index.js
+++ restful-api/index.js
@@ -7,14 +7,18 @@ const http = require('http');
 const url = require('url');
 
 // The server should respond to all requests with a string
-const server = http.createServer(function(req, res) {
-  
+const server = http.createServer(function (req, res) {
+
   // Get the URL and parse it
   const parsedUrl = url.parse(req.url, true);
 
   // Get the path
   const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\+|\/+$/g,'');
+  const trimmedPath = path.replace(/^\+|\/+$/g, '');
+
+  // Get the query string as an object
+  // ex: localhost:3000/foo?fizz=buzz
+  const queryStringObject = parsedUrl.query;
 
   // Get the HTTP Method
   const method = req.method.toLowerCase();
@@ -23,11 +27,16 @@ const server = http.createServer(function(req, res) {
   res.end('Hello World!\n');
 
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath + ' with method: ' + method);
-  
+  console.log(
+    'Request received on path: '
+    + trimmedPath + ' with method: '
+    + method + ' and with these query strings parameters',
+    queryStringObject
+  );
+
 });
 
 // Start the server, and have it listen on port 3000
-server.listen(3000, function() {
+server.listen(3000, function () {
   console.log("The server is listening on port 3000 now");
 });
